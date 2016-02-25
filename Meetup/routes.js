a03036ba72f9b4af28471e842cc884c0123c6c13
Bogdan Hossu <bogdanhossu@Bogdans-MacBook-Pro.local>
2016-02-25 12:12:01 +0100

    Please work

---
+++ Meetup/routes.js
@@ -0,0 +1,35 @@
+// /routes.js
+
+// grab the meet model we just created
+var Meet = require('./models/meet');
+
+    module.exports = function(app) {
+
+        // server routes ===========================================================
+        // handle things like api calls
+        // authentication routes
+
+        // sample api route
+        app.get('/api/meets', function(req, res) {
+            // use mongoose to get all meets in the database
+            Meet.find(function(err, meets) {
+
+                // if there is an error retrieving, send the error. 
+                                // nothing after res.send(err) will execute
+                if (err)
+                    res.send(err);
+
+                res.json(meets); // return all meets in JSON format
+            });
+        });
+
+        // route to handle creating goes here (app.post)
+        // route to handle delete goes here (app.delete)
+
+        // frontend routes =========================================================
+        // route to handle all angular requests
+        app.get('*', function(req, res) {
+            res.sendfile('./public/views/index.html'); // load our public/index.html file
+        });
+
+    };
